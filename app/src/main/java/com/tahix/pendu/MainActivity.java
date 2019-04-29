34de317ac1033eb43d187811d02d1b14be1b9e9f
Tahix <42356166+yahyatahix@users.noreply.github.com>
2019-04-29 09:26:51 +0100

    cmd vocale

--- app/src/main/java/com/tahix/pendu/MainActivity.java
+++ app/src/main/java/com/tahix/pendu/MainActivity.java
@@ -1,12 +1,18 @@
 package com.tahix.pendu;
 
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,15 +33,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView image;
     private EditText letter ;
     private String word ;
+    private String alphabet;
     private int found ;
     private int error ;
     private List<Character> listOfLetters = new ArrayList<>();
     private boolean win ;
     private List<String> wordlist = new ArrayList<>();
-
+    private ImageButton btnVoice;
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -47,7 +57,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initGame();
         btnSend.setOnClickListener(this);
 
+        btnVoice = (ImageButton) findViewById(R.id.btnVoice);
+        btnVoice.setOnClickListener(new View.OnClickListener() {
+            private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
 
+            @Override
+            public void onClick(View v) {
+                speak();
+
+            }
+
+            private void speak() {
+                /*Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi  speak something ");
+
+                try {
+                    startActivityForResult(intent,REQUEST_CODE_SPEECH_INPUT);
+                }
+                catch (Exception e){
+                    Toast.makeText( getApplicationContext() ," "+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }*/
+
+                try {
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
+                    startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+                    intent.putExtra("letter",alphabet) ;
+                    alphabet = intent.getStringExtra("letter");
+                }
+                catch(ActivityNotFoundException e) {
+                    String appPackageName = "com.google.android.googlequicksearchbox";
+                    try {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                    } catch (android.content.ActivityNotFoundException anfe) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                    }
+                }
+            }
+            public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+                MainActivity.super.onActivityResult(requestCode, resultCode, data);
+
+                switch (requestCode){
+                    case REQUEST_CODE_SPEECH_INPUT:{
+                        if (requestCode == RESULT_OK && null!=data){
+                            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                            letter.setText(result.get(0));
+                        }
+                        break;
+                    }
+                }
+            }
+
+
+        });
     }
 
     public void initGame(){
@@ -71,13 +137,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v){
-        String letterFromInput = letter.getText().toString().toUpperCase();
+
+        alphabet = letter.getText().toString().toUpperCase();
         letter.setText("");
 
-        if (letterFromInput.length() > 0){
-            if (!letterAlreadyUsed(letterFromInput.charAt(0), listOfLetters)){
-                listOfLetters.add(letterFromInput.charAt(0));
-                checkIfLetterIsInWord(letterFromInput, word);
+
+        //lettreTapez.setText(alphabet);
+        if (alphabet.length() > 0){
+            if (!letterAlreadyUsed(alphabet.charAt(0), listOfLetters)){
+                listOfLetters.add(alphabet.charAt(0));
+                checkIfLetterIsInWord(alphabet, word);
             }
             //la partie est gagné
             if (found == word.length()){
@@ -86,7 +155,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
             //la lettrre n'est pas dans le mot
-            if (!word.contains(letterFromInput)){
+            if (!word.contains(alphabet)){
                 error++;
             }
             setImage(error);
@@ -98,6 +167,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // Affichage des lettres entrées
             showAllLetters(listOfLetters);
         }
+
 
     }
     public boolean letterAlreadyUsed(char a, List<Character> listOfletters) {
